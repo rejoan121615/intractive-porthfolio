68883f7354f8cecbf7d01b560aacefaa7d49a04c
Mohd Rejoan <wearecrx@gmail.com>
2021-08-14 20:45:56 +0600

    name animation added

--- app/js/script.js
+++ app/js/script.js
@@ -1,20 +1,50 @@
-'use strict';
-import '../scss/style.scss'
-import gsap from 'gsap';
+"use strict";
+import "../scss/style.scss";
+import gsap from "gsap";
+import Splitting from 'splitting';
+
+// split texts 
+Splitting()
 
 gsap.defaults({
-    duration: 1.4
-})
-// animation 
+    duration: 1,
+});
+// animation
+
 gsap.timeline().to("#intro .cover", {
     x: 0,
+    stagger: {
+        amount: 1.3,
+    },
     delay: 0.5,
-    ease: 'ease-in'
-}).to("#intro .dot", {
-    width: 105 + 'vw',
-    height: 105 + 'vh',
-}).to("#intro .dot", {
-    borderRadius: 0,
-}, '-=0.4').to("#intro", {
-    display: 'none'
+    duration: 1.3
+}).to("#intro .line", {
+    height: 0,
+    delay: 0.4,
+    stagger: {
+        amount: 0.4,
+        from: 'end'
+    },
+    onComplete: function () {
+        document.querySelector("#intro").remove()
+        startNameAnimation()
+    }
 })
+// startNameAnimation()
+
+// name animation 
+function startNameAnimation() {
+    let nameTl = gsap.timeline()
+    nameTl.to("body", {
+        overflowY: 'initial'
+    }).to("#name", {
+        display: 'flex'
+    }).from(gsap.utils.toArray(['.line_two .char', '.line_one .char']), {
+        y: -300,
+        opacity: 0,
+        stagger: {
+            amount: 0.8,
+        }
+    })
+    return nameTl;
+}
